5a4b590ec37e6905656bf3888656a44bf56f5104
litsey99 <qwaszxc9099@gmail.com>
2024-11-11 13:50:51 +0600

    Update script.js
    3

--- script.js
+++ script.js
@@ -1,43 +1,140 @@
-const modal = document.getElementById('modal');
-const sign = document.getElementById('sign');
-const login = document.getElementById('login');
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-const createForm = (type) => `
-<form action="action_page.php">
-  <div class="container">
-    <h1>${type}</h1>
-    <p>Please fill in this form to ${type}.</p>
-    <hr>
-    <label for="${type}-email"><b>Email</b></label>
-    <div class="input-container">
-      <i class='bx bx-user'></i>
-      <input type="text" placeholder="Enter Email" name="email" id="${type}-email" required>
-    </div>
-    <label for="${type}-psw"><b>Password</b></label>
-    <div class="input-container">
-      <i class='bx bx-lock'></i>
-      <input type="password" placeholder="Enter Password" name="psw" id="${type}-psw" required>
-    </div>
-    ${type === 'signup' ? `
-      <label for="${type}-psw-repeat"><b>Repeat Password</b></label>
-      <div class="input-container">
-        <i class='bx bx-lock'></i>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="${type}-psw-repeat" required>
-      </div>
-      <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-    ` : ''}
-    <button type="submit" class="registerbtn">${type === 'login' ? 'Log In' : 'Register'}</button>
-  </div>
-</form>
-`;
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
 
-login.addEventListener('click', () => {
-  modal.classList.toggle('vis');
-  modal.innerHTML = createForm('login');
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
-sign.addEventListener('click', () => {
-  modal.classList.toggle('vis');
-  modal.innerHTML = createForm('signup');
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
+const scrollRevealOption = {
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__container form", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__container img", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".range__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".location__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".location__content .section__logo1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".location__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".location__content .location__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+const selectCards = document.querySelectorAll(".select__card");
+selectCards[0].classList.add("show__info");
+
+const price = ["225", "455", "275", "625", "395"];
+
+const priceEl = document.getElementById("select-price");
+
+function updateSwiperImage(eventName, args) {
+  if (eventName === "slideChangeTransitionStart") {
+    const index = args && args[0].realIndex;
+    priceEl.innerText = price[index];
+    selectCards.forEach((item) => {
+      item.classList.remove("show__info");
+    });
+    selectCards[index].classList.add("show__info");
+  }
+}
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    depth: 500,
+    modifier: 1,
+    scale: 0.75,
+    slideShadows: false,
+    stretch: -100,
+  },
+
+  onAny(event, ...args) {
+    updateSwiperImage(event, args);
+  },
+});
+
+ScrollReveal().reveal(".story__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const banner = document.querySelector(".banner__wrapper");
+
+const bannerContent = Array.from(banner.children);
+
+bannerContent.forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  banner.appendChild(duplicateNode);
+});
+
+ScrollReveal().reveal(".download__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".download__content .section__logo1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".download__links", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+
+// перевод 
+document.getElementById("language-switch").addEventListener("change", function(event) {
+  // Получаем значение выбранного языка (например, ?hl=en)
+  let lang = event.target.value;
+
+  // Перезагружаем страницу с новым параметром языка
+  window.location.search = lang;
+});
+
+// Функция для загрузки контента на выбранном языке
+function loadContent() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const language = urlParams.get('hl') || 'en'; // Если язык не выбран, по умолчанию 'en'
+
+  // Логика перевода контента
+  translatePage(language);
+}
